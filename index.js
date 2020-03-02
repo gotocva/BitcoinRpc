b83cc92dcb5640ef1e01dd8f11968cbfbbaf9eb9
Sivabharathy <gotocva@gmail.com>
2020-03-02 13:44:47 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,121 @@
+const rpc = require('node-json-rpc');
+const BlockCyper = require("./BlockCyper");
+ 
+const options = {
+    // int port of rpc server, default 5080 for http or 5433 for https
+    port: process.env.RPC_PORT || 5080,
+    // string domain name or ip of rpc server, default '127.0.0.1'
+    host: process.env.RPC_HOST || '127.0.0.1',
+    // string with default path, default '/'
+    path: process.env.RPC_PATH || '/',
+    // boolean false to turn rpc checks off, default true
+    strict: true
+};
+console.log(options)
+// Create a server object with options
+const server = new rpc.Server(options);
+
+server.addMethod('CREATE_ADDRESS', async function(args,cb){
+    const bcyper = new BlockCyper('btc','test3');
+    await bcyper.createAddress()
+    .then((address) => {
+        cb(null,address);
+    })
+    .catch((error) => {
+        cb(error,null);
+    });
+});
+
+server.addMethod('CREATE_TRANSACTION', async function(args,cb){
+    const bcyper = new BlockCyper('btc','test3'); 
+
+    if(args.amount < 0.0001){
+        cb({"status":false,"error_message":"Minimum amount for transaction is 0.0001"},null);
+    }else{
+        let amount = 100000000 * args.amount;
+        let tx = {"inputs": [{"addresses": [args.from]}], "outputs": [{"addresses": [args.to], "value": amount}]};
+        await bcyper.transaction(tx,args.private_key)
+        .then((result) => {
+            cb(null,result.tx);
+        })
+        .catch((error) => {
+            cb(error,null);
+        });
+    }
+    
+});
+
+server.addMethod('ping', async function (args, cb) {
+    console.dir(args);
+    cb(null,{"status":true});
+});
+
+server.start(function (error) {
+    // Did server start succeed ?
+    if (error) throw error;
+    else {
+      console.log('Server running ...');
+    }
+});
+
+
+// server.addMethod('sendTokens', async function (args, cb) {
+//     let error, result, cAddress, address, password, to, amount;
+//     try {
+//         if (args.length === 6) {
+//             cAddress = args[0];
+//             address = args[1];
+//             password = args[2];
+//             to = args[3];
+//             amount = args[4];
+//             coin_value = args[5];
+//             result = await cntrctInterface.sendTokens(cAddress, address, password, to, amount, coin_value);
+//             cb(error, result);
+//         }
+//         else {
+//             throw new Error('Invalid Params');
+//         }
+//         cb(error, result);
+//     }
+//     catch (ex) {
+//         cb({ code: 'INTERNAL ERROR', message: ex.message });
+//     }
+// });
+// server.addMethod('getMyTokenBalance', async function (args, cb) {
+//     let error, result, cAddress, address;
+//     try {
+//         if (args.length !== 2) throw new Error('Invalid Params');
+//         cAddress = args[0];
+//         address = args[1];
+//         result = await cntrctInterface.getBalance(cAddress, address);
+//         cb(error, result);
+//     }
+//     catch (ex) {
+//         cb({ code: 'INTERNAL ERROR', message: ex.message });
+//     }
+// });
+// server.addMethod('createAddressForToken', async function (args, cb) {
+//     let error, result, password;
+//     try {
+//         if (args.length !== 1) throw new Error('Invalid Params');
+//         password = args[0];
+//         result = await cntrctInterface.createAddress(password);
+//         cb(error, result);
+//     }
+//     catch (ex) {
+//         cb({ code: 'INTERNAL ERROR', message: ex.message });
+//     }
+// });
+// server.addMethod('personal_newAccount', async function (args, cb) {
+//     let error, result, password;
+//     try {
+//         if (args.length !== 1) throw new Error('Invalid Params');
+//         password = args[0];
+//         result = await cntrctInterface.createAddress(password);
+//         cb(error, result);
+//     }
+//     catch (ex) {
+//         cb({ code: 'INTERNAL ERROR', message: ex.message });
+//     }
+// });
+// module.exports = { rpcServer: server, rpcClient: client };
